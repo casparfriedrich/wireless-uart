18ef4f969a0cc2ef7eda40bb4b776616209cfa6f
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2019-09-15 18:16:01 +0200

    Add status flash via timer

--- application/src/main.c
+++ application/src/main.c
@@ -14,6 +14,13 @@ extern const k_tid_t serial_thread;
 K_MSGQ_DEFINE(esb_frame_q, sizeof(struct nrf_esb_payload), 100, 4);
 K_MSGQ_DEFINE(serial_frame_q, sizeof(struct nrf_esb_payload), 100, 4);
 
+static void status_led_expiry_fn(struct k_timer *timer)
+{
+	led_flash(LED_0);
+}
+
+static K_TIMER_DEFINE(status_led_timer, status_led_expiry_fn, NULL);
+
 void hf_clock_init(void)
 {
 	nrf_clock_task_trigger(NRF_CLOCK_TASK_HFCLKSTART);
@@ -27,17 +34,11 @@ void main(void)
 {
 	LOG_INF("Starting Application: %p", k_current_get());
 
+	k_timer_start(&status_led_timer, 0, K_SECONDS(1));
+
 	hf_clock_init();
 	led_init();
 
 	k_thread_start(esb_thread);
 	k_thread_start(serial_thread);
-
-	while (1) {
-		led_flash(LED_0);
-		k_sleep(K_MSEC(1000));
-	}
 }
-
-
-
