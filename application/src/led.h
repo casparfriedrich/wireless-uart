c22b8800815abb042df82c5ecde1a5b663f81702
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2020-04-28 22:25:27 +0200

    Let the kernel initialize leds

--- application/src/led.h
+++ application/src/led.h
@@ -1,13 +1,8 @@
 #ifndef LEDS_H
 #define LEDS_H
 
-enum led {
-	LED_RED,
-	LED_GREEN,
-	LED_BLUE
-};
+enum led { LED_RED, LED_GREEN, LED_BLUE };
 
-void led_init(void);
 void led_flash(enum led led);
 
 #endif // LEDS_H
