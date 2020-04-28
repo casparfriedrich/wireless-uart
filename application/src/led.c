de8255723c2350bffabd0b15141c38760e5231a3
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2020-04-28 21:17:33 +0200

    Fix deprecated gpio api

--- application/src/led.c
+++ application/src/led.c
@@ -14,28 +14,25 @@ static void _led_init(const char *device_name, u32_t led_pin)
 {
 	struct device *led_controller = device_get_binding(device_name);
 	gpio_pin_configure(led_controller, led_pin, GPIO_DIR_OUT);
-	gpio_pin_write(led_controller, led_pin, LED_OFF);
+	gpio_pin_set(led_controller, led_pin, LED_OFF);
 }
 
 static void _timer_0_expiry_fn(struct k_timer *timer)
 {
-	gpio_pin_write(device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER),
-	               DT_ALIAS_LED0_GPIOS_PIN,
-	               LED_OFF);
+	gpio_pin_set(device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER),
+		     DT_ALIAS_LED0_GPIOS_PIN, LED_OFF);
 }
 
 static void _timer_1_expiry_fn(struct k_timer *timer)
 {
-	gpio_pin_write(device_get_binding(DT_ALIAS_LED1_GPIOS_CONTROLLER),
-	               DT_ALIAS_LED1_GPIOS_PIN,
-	               LED_OFF);
+	gpio_pin_set(device_get_binding(DT_ALIAS_LED1_GPIOS_CONTROLLER),
+		     DT_ALIAS_LED1_GPIOS_PIN, LED_OFF);
 }
 
 static void _timer_2_expiry_fn(struct k_timer *timer)
 {
-	gpio_pin_write(device_get_binding(DT_ALIAS_LED2_GPIOS_CONTROLLER),
-	               DT_ALIAS_LED2_GPIOS_PIN,
-	               LED_OFF);
+	gpio_pin_set(device_get_binding(DT_ALIAS_LED2_GPIOS_CONTROLLER),
+		     DT_ALIAS_LED2_GPIOS_PIN, LED_OFF);
 }
 
 static K_TIMER_DEFINE(_led_timer_0, _timer_0_expiry_fn, _timer_0_expiry_fn);
@@ -57,22 +54,25 @@ void led_flash(enum led led)
 
 	switch (led) {
 	case LED_RED:
-		led_controller = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+		led_controller =
+			device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
 		led_pin = DT_ALIAS_LED0_GPIOS_PIN;
 		led_timer = &_led_timer_0;
 		break;
 	case LED_GREEN:
-		led_controller = device_get_binding(DT_ALIAS_LED1_GPIOS_CONTROLLER);
+		led_controller =
+			device_get_binding(DT_ALIAS_LED1_GPIOS_CONTROLLER);
 		led_pin = DT_ALIAS_LED1_GPIOS_PIN;
 		led_timer = &_led_timer_1;
 		break;
 	case LED_BLUE:
-		led_controller = device_get_binding(DT_ALIAS_LED2_GPIOS_CONTROLLER);
+		led_controller =
+			device_get_binding(DT_ALIAS_LED2_GPIOS_CONTROLLER);
 		led_pin = DT_ALIAS_LED2_GPIOS_PIN;
 		led_timer = &_led_timer_2;
 		break;
 	}
 
-	gpio_pin_write(led_controller, led_pin, LED_ON);
+	gpio_pin_set(led_controller, led_pin, LED_ON);
 	k_timer_start(led_timer, K_MSEC(50), 0);
 }
